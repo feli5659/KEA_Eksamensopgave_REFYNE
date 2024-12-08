043c7c840da1d69ca4a1f569037d8f4cd37bec7b
felicia <feli5659@stud.kea.dk>
2024-12-08 09:57:25 +0100

    Oversæt til dansk

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const contentTemplates = {
   fashion: {
-    h3: "Performance marketing agency with expertise in fashion",
-    h1: "Transform your B2C fashion brand with our innovative solution",
-    p: "Discover how we can help you achieve your B2C fashion brand goals faster and more efficiently than ever before",
-    cta: "Contact us to hear what we can do for your fashion brand",
+    h3: "Performance marketing bureau med expertise i fashion",
+    h1: "Skalér jeres B2C fashion brand med datadrevet marketing.",
+    p: "Vi forstår de unikke udfordringer ved at markedsføre modebrands i et konkurrencepræget marked. Vores skræddersyede løsninger sikrer, at dit brand når de rette kunder, opbygger loyalitet og skaber målbare resultater.",
+    cta: "Kontat os for hjælp til dit fashion brand",
   },
   tech: {
-    h3: "Performance marketing agency with expertise in SaaS",
-    h1: "Transform your B2B tech startup with our innovative solution",
-    p: "Discover how we can help you achieve your SaaS startup goals faster and more efficiently than ever before",
-    cta: "Contact us to hear what we can do for your tech startup",
+    h3: "Performance marketing bureau med expertise i SaaS",
+    h1: "Skalér din B2B tech startup med datadrevet marketing.",
+    p: "Som en del af en hurtigt udviklende tech-branche er det afgørende at være foran konkurrenterne. Vi hjælper din virksomhed med at skabe relevant og engagerende kommunikation, der konverterer leads til loyale kunder gennem præcist målrettet marketing.",
+    cta: "Kontat os for hjælp til din tech startup",
   },
   health: {
-    h3: "Performance marketing agency with expertise in healthcare",
-    h1: "Transform your healthcare business with our innovative solution",
-    p: "Discover how we can help you achieve your healthcare company goals faster and more efficiently than ever before",
-    cta: "Contact us to hear what we can do for your healthcare company",
+    h3: "Performance marketing bureau for B2B-virksomheder",
+    h1: "Skab vækst i din B2B-virksomhed med målrettede kampagner",
+    p: "Vi hjælper B2B-virksomheder med at nå beslutningstagere, opbygge stærke relationer og generere flere leads gennem strategisk marketing.",
+    cta: "Kontat os for hjælp til jeres B2B virksomhed",
   },
 };
 
@@ -29,5 +29,5 @@ document.getElementById("searchSimulator").addEventListener("change", function (
   document.getElementById("dynamicH3").textContent = content.h3;
   document.getElementById("dynamicH1").textContent = content.h1;
   document.getElementById("dynamicP").textContent = content.p;
-  document.getElementById("dynamicCTA").innerHTML = content.cta + ' <span class="arrow">→</span>';
+  document.getElementById("dynamicCTA").textContent = content.cta;
 });
